b51fd975c3fd079503abd172745bfd724a15e852
Parmar1001 <chparmar@bestpeers.com>
2022-02-23 12:51:45 +0530

    all auth login signup form completed

---
+++ MyApp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.models import User
+from MyApp.models import Customer
+
+class SignUpForm(forms.ModelForm):
+    class Meta: 
+        model=User
+        fields=['username','password','email','first_name','last_name']
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model=Customer
+        fields=['profile_pic','balance','account_type','state','city','branch_name']
