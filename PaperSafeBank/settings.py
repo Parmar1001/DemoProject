bb801984250f9f4c4ad5c119486c89ff12967ee8
Parmar1001 <chparmar@bestpeers.com>
2022-03-25 12:19:27 +0530

    Initial commit

--- PaperSafeBank/settings.py
+++ PaperSafeBank/settings.py
@@ -104,10 +104,14 @@ WSGI_APPLICATION = "PaperSafeBank.wsgi.application"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'customer',
+       'USER': 'demo',
+       'PASSWORD': 'demo',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
