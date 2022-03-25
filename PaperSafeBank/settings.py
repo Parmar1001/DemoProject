0b7d0dadbb54b7a03f9e78171f780f935302870c
Parmar1001 <chparmar@bestpeers.com>
2022-03-25 15:09:17 +0530

    3nd commit

--- PaperSafeBank/settings.py
+++ PaperSafeBank/settings.py
@@ -107,11 +107,12 @@ WSGI_APPLICATION = "PaperSafeBank.wsgi.application"
 DATABASES = {
    'default': {
        'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'customer',
-       'USER': 'demo',
-       'PASSWORD': 'demo',
-       'HOST': 'localhost',
-       'PORT': '5432',
+       'NAME': 'dfroc02107jgn',
+       'USER': 'ysbzrvnegodgxq',
+       'PASSWORD': 'cffdf4578d9b9f8abde4c59f2e072d93379ba1c248825bbb9dea8b9c1ce04527',
+       'HOST': 'ec2-18-215-96-22.compute-1.amazonaws.com',
+       'PORT': 5432,
+       postgres://ysbzrvnegodgxq:cffdf4578d9b9f8abde4c59f2e072d93379ba1c248825bbb9dea8b9c1ce04527@ec2-18-215-96-22.compute-1.amazonaws.com:5432/dfroc02107jgn
    }
 }
 
